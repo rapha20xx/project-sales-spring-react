75fb46be6766fc067d29c586559b7cd342ee5e9c
Raphael Santos <raphaelsantos0712@gmail.com>
2021-09-12 14:39:44 -0300

    BarChart Integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -23,17 +23,13 @@ const DonutChart = () => {
 
                 setChartData({ labels: myLabels, series: mySeries });
             });
-    }, [])
+    }, []);
 
     //Forma zuada
 
 
 
-    // const mockData = {
-    //   series: [477138, 499928, 444867, 220426, 473088],
-    // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    // }
-
+    
     const options = {
         legend: {
             show: true
